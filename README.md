0f75d02a17d94fe43ab3e71b490d978d2389c523
Phad-Kishore1998 <45930935+Phad-Kishore1998@users.noreply.github.com>
2025-03-09 16:21:19 +0000

    In Progress

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ToDoList1506
+#  npx create-react-app movieapp
+
+Local installation and then 
+
+### cd movieapp
+### npm start for starting the app.
+
+Deleted except App.js index.js and index.css
---
+++ movieapp/src/App.js
@@ -0,0 +1,18 @@
+import MovieCard from "./MovieCard.js"
+function App() {
+  return (
+    <>
+    <h1>Movie App</h1>
+    < MovieCard/>
+    </>
+  );
+}
+
+export default App;
+
+
+/* Notes for main file.
+
+
+
+*/
